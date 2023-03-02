6656b2897f3ba6154137532616ffbc72e5ad4677
RubyRode <dimaskrut71@gmail.com>
2023-03-02 18:38:32 +0700

    Heat equation implemented

--- Task1/code_cpu1.cpp
+++ Task1/code_cpu1.cpp
@@ -15,9 +15,9 @@ public:
             if (arg == "-accur"){
                 std::string dump = std::string(argv[i+1]);
                 this->_accur = std::stod(dump);
-            }else if (arg == "grid"){
+            }else if (arg == "-grid"){
                 this->_grid_size = std::stoi(argv[i + 1]);
-            }else if (arg == "i"){
+            }else if (arg == "-i"){
                 this->_iters = std::stoi(argv[i + 1]);
             }
         }
@@ -46,7 +46,7 @@ private:
     int size;
     double corners[4] = {10, 20, 30, 20};
     double accuracy;
-    int iters;
+    int _iters;
 
 public:
     explicit Grid(parser input){
@@ -57,11 +57,32 @@ public:
         this->grid[this->size-1][0] = this->corners[2];
         this->grid[this->size-1][this->size-1] = this->corners[3];
         this->accuracy = input.accuracy();
-        this->iters = input.iterations();
+        this->_iters = input.iterations();
+    }
+
+    void init(){
+        for (int i=1; i<this->size-1; i++){
+            this->grid[0][i] = this->grid[0][i-1] + ((this->corners[1]-this->corners[0])/(this->size-1));
+            this->grid[this->size-1][i] = this->grid[this->size-1][i-1] + ((this->corners[3]-this->corners[2])/(this->size-1));
+            this->grid[i][0] = this->grid[i-1][0] + ((this->corners[2]-this->corners[0])/(this->size-1));
+            this->grid[i][this->size-1] = this->grid[i-1][this->size-1] + ((this->corners[3]-this->corners[1])/(this->size-1));
+        }
+
+    }
+
+    void fill(){
+        for (int i=1; i<this->size-1; i++){
+            for (int j=1; j<this->size-1; j++){
+                this->grid[i][j] = (this->grid[i-1][j] + this->grid[i][j-1] + this->grid[i][j+1] + this->grid[i+1][j])/4;
+            }
+        }
     }
 
     [[nodiscard]] double grid_val(int i, int j) const{
         return this->grid[i][j];
+    }
+    [[nodiscard]] int iters() const{
+        return this->_iters;
     }
 
 };
@@ -70,6 +91,20 @@ public:
 int main(int argc, char ** argv){
     parser input = parser(argc, argv);
     Grid gr(input);
+
+
+    gr.init();
+    for (int i=0; i<gr.iters(); i++) {
+        gr.fill();
+    }
+    for (int i=0; i<input.grid(); i++){
+        for (int j=0; j<input.grid(); j++){
+            fprintf(stdout, "%0.2f  " , gr.grid_val(i, j));
+        }
+        std::cout << std::endl;
+    }
+
+    
 
     return 0;
 }
