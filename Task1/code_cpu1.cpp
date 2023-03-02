54e47067b2830f098323102e2ca5b7c8b9d99e46
RubyRode <dimaskrut71@gmail.com>
2023-03-03 00:41:35 +0700

    Time measures added

--- Task1/code_cpu1.cpp
+++ Task1/code_cpu1.cpp
@@ -15,7 +15,7 @@ public:
             if (arg == "-accur"){
                 std::string dump = std::string(argv[i+1]);
                 this->_accur = std::stod(dump);
-            }else if (arg == "-grid"){
+            }else if (arg == "-a"){
                 this->_grid_size = std::stoi(argv[i + 1]);
             }else if (arg == "-i"){
                 this->_iters = std::stoi(argv[i + 1]);
@@ -42,48 +42,75 @@ private:
 class Grid{
 private:
 
-    std::vector<std::vector<double>> grid;
+    std::vector<std::vector<double>> a;
+    std::vector<std::vector<double>> a_new;
     int size;
     double corners[4] = {10, 20, 30, 20};
-    double accuracy;
+    double _accuracy;
     int _iters;
 
 public:
     explicit Grid(parser input){
         this->size = input.grid();
-        this->grid = std::vector<std::vector<double>>(this->size, std::vector<double>(this->size, 0));
-        this->grid[0][0] = this->corners[0];
-        this->grid[0][this->size-1] = this->corners[1];
-        this->grid[this->size-1][0] = this->corners[2];
-        this->grid[this->size-1][this->size-1] = this->corners[3];
-        this->accuracy = input.accuracy();
+        this->a = std::vector<std::vector<double>>(this->size, std::vector<double>(this->size, 0));
+        this->a_new = std::vector<std::vector<double>>(this->size, std::vector<double>(this->size, 0));
+        this->a_new[0][0] = this->a[0][0] = this->corners[0];
+        this->a_new[0][this->size - 1] = this->a[0][this->size - 1] = this->corners[1];
+        this->a_new[this->size - 1][0] = this->a[this->size - 1][0] = this->corners[3];
+        this->a_new[this->size - 1][this->size - 1] = this->a[this->size - 1][this->size - 1] = this->corners[2];
+        this->_accuracy = input.accuracy();
         this->_iters = input.iterations();
     }
 
-    void init(){
-        for (int i=1; i<this->size-1; i++){
-            this->grid[0][i] = this->grid[0][i-1] + ((this->corners[1]-this->corners[0])/(this->size-1));
-            this->grid[this->size-1][i] = this->grid[this->size-1][i-1] + ((this->corners[3]-this->corners[2])/(this->size-1));
-            this->grid[i][0] = this->grid[i-1][0] + ((this->corners[2]-this->corners[0])/(this->size-1));
-            this->grid[i][this->size-1] = this->grid[i-1][this->size-1] + ((this->corners[3]-this->corners[1])/(this->size-1));
+    void fill(){
+
+        double step = (this->corners[1] - this->corners[0]) / (this->size - 1);
+        #pragma acc data copy(this->a[0:this->size]), copy(this->a_new[0:this->size])
+        {
+            #pragma acc parallel loop seq gang num_gangs(256) vector vector_length(256)
+            for (int i=1; i<this->size-1; i++){
+                this->a_new[0][i] = this->a[0][i] = this->a[0][i - 1] + step;
+                this->a_new[this->size - 1][i] = this->a[this->size - 1][i] = this->a[this->size - 1][i - 1] + step;
+                this->a_new[i][0] = this->a[i][0] = this->a[i - 1][0] + step;
+                this->a_new[i][this->size-1] = this->a[i][this->size - 1] = this->a[i - 1][this->size - 1] + step;
+            }
         }
+
 
     }
 
-    void fill(){
-        #pragma acc parallel loop
+    double update(double error){
+        error = 0.0;
+
+        #pragma acc parallel loop seq vector vector_length(this->size) gang num_gangs(256) reduction(max:error) \
+            present(a[0:this->size], a_new[0:this->size])
         for (int i=1; i<this->size-1; i++){
             for (int j=1; j<this->size-1; j++){
-                this->grid[i][j] = (this->grid[i-1][j] + this->grid[i][j-1] + this->grid[i][j+1] + this->grid[i+1][j])/4;
+                this->a[i][j] = (this->a[i - 1][j] + this->a[i][j - 1] + this->a[i][j + 1] + this->a[i + 1][j]) / 4;
+                error = fmax(error, this->a[i][j] - this->a_new[i][j]);
+            }
+        }
+        return error;
+    }
+
+    void sw_ap(){
+        #pragma acc parallel loop seq vector vector vector_length(this->size) gang num_gangs(256) \
+        present(this->a[0:this->size], this->a_new[0:this->size])
+        for (int i=1; i<this->size-1; i++){
+            for (int j=1; j<this->size-1; j++){
+                this->a_new[i][j] = this->a[i][j];
             }
         }
     }
 
     [[nodiscard]] double grid_val(int i, int j) const{
-        return this->grid[i][j];
+        return this->a[i][j];
     }
     [[nodiscard]] int iters() const{
         return this->_iters;
+    }
+    [[nodiscard]] double accuracy() const{
+        return this->_accuracy;
     }
 
 };
@@ -93,19 +120,30 @@ int main(int argc, char ** argv){
     parser input = parser(argc, argv);
     Grid gr(input);
 
-    gr.init();
-    #pragma acc data copy(gr)
-    for (int i=0; i<gr.iters(); i++) {
-        gr.fill();
-    }
-    for (int i=0; i<input.grid(); i++){
-        for (int j=0; j<input.grid(); j++){
-            fprintf(stdout, "%0.2f  " , gr.grid_val(i, j));
-        }
-        std::cout << std::endl;
+    clock_t start = clock();
+
+    gr.fill();
+
+    clock_t end = clock();
+    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
+
+    std::cout << "Initialization time: " << elapsed_secs << std::endl;
+
+    double error = 1;
+    int g;
+    start = clock();
+
+    for (int i = 0; i < gr.iters() && error > gr.accuracy(); i++) {
+         error = gr.update(error);
+         gr.sw_ap();
+         g = i;
     }
 
-    
+    end = clock();
+    elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
 
+    std::cout << "Computations time: " << elapsed_secs << std::endl;
+    std::cout << "Error: " << error << std::endl;
+    std::cout << "Iteration: " << g+1 << std::endl;
     return 0;
 }
